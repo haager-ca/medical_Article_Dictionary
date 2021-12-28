cbca9572253a8be75fc400e507c9bbc48e070c22
haager-ca <caius.haager@icloud.com>
2021-12-28 23:56:35 +0100

    added new pipe

---
+++ src/app/_pipes/join-authors.pipe.ts
@@ -0,0 +1,17 @@
+import { ValueTransformer } from '@angular/compiler/src/util';
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'joinAuthors'
+})
+export class JoinAuthorsPipe implements PipeTransform {
+
+  transform(value: string[], ...args: unknown[]): unknown {
+    if(value.length > 1) {
+      const lastAuthor = value.pop();
+      return value.join(", ") + " und " + lastAuthor;
+    }
+    return value.join(", ");
+  }
+
+}
